41ceccd407a9b8ba08927a124cfe1178d214818a
Crowdbotics <team@crowdbotics.com>
2022-06-07 18:11:57 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/joind_health_35636/MainActivity.java
@@ -0,0 +1,15 @@
+package com.joind_health_35636;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "joind_health_35636";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "joind_health_35636",
+  url: "https://joind_health_35636.botics.co",
+  api: "https://joind_health_35636.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
